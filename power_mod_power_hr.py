33d5bc7cc9132e36f8fc8541cd7bc054123cd913
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-11-01 23:35:51 +0530

    Add files via upload

---
+++ power_mod_power_hr.py
@@ -0,0 +1,19 @@
+'''
+power-mod power problem in python (HackerRank)
+'''
+
+'''method 1'''
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# p=pow(a,b)
+# m=pow(a,b,c)
+# print(p)
+# print(m)
+
+'''method 2'''
+# a,b,c=3,4,10
+# p=pow(a,b)
+# m=pow(a,b,c)
+# print(p)
+# print(m)
