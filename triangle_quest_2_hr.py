2ef5348874113832f6dc4e9ab91b2de3d1ca8c08
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-11-04 23:18:34 +0530

    Update triangle_quest_2_hr.py

--- triangle_quest_2_hr.py
+++ triangle_quest_2_hr.py
@@ -1,2 +1,10 @@
-for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
-    print(((10**i - 1)//9)**2)
+'''triangle quest 2'''
+
+'''method 1'''
+# n=int(input())
+# for i in range (1,n+1):
+#     print(((10**i-1)//9)**2)
+
+'''method 2'''
+# for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+#     print(((10**i-1)//9)**2)
