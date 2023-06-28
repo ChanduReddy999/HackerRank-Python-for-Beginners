cad1e68cd83b6cd3054856b7c68de5b5e6194972
V CHANDRA SHEKAR REDDY <113517229+ChanduReddy999@users.noreply.github.com>
2023-06-28 15:39:50 +0530

    Add files via upload

---
+++ loop_problem_hr.py
@@ -0,0 +1,17 @@
+"""
+loop problem in python hackerrank
+"""
+
+"""Method 1"""
+
+# n = int(input())
+# for i in range(0,n):
+#     print(i*i)
+
+
+'''method 2'''
+
+# n=int(input())
+# for i in range(0,n):
+#     square=i*i
+#     print(square)
